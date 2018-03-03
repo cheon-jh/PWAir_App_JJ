2bd48b24f61ab9b0212abaec923ef3680eaf3079
천정훈 <csdcij@gmail.com>
2018-03-03 16:55:40 +0900

    commit

---
+++ src/components/login/Login.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="">
+  <LoginHeader></LoginHeader>
+  <LoginForm></LoginForm>
+  <LoginSNS></LoginSNS>
+</div>
+</template>
+
+<script>
+import LoginForm from './LoginForm.vue'
+import LoginHeader from './LoginHeader.vue'
+import LoginSNS from './LoginSNS.vue'
+
+export default {
+  components:{
+    LoginHeader,
+    LoginForm,
+    LoginSNS,
+  }
+}
+</script>
+
+<style>
+</style>
